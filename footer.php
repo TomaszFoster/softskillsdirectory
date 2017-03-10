6285ac48c7b47bbb817c5f6863d520b24b7e687f
Tomasz Foster <tomaszfoster@gmail.com>
2017-03-09 23:00:43 -0500

    updated order of items and locally hosting masonry instead of cdn

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 	    <script src="/js/modernizr.js"></script>
 		<script src="/js/jquery.js"></script>
 		<script src="/js/foundation.min.js"></script>
-		<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+		<script src="/js/masonry.pkgd.min.js"></script>
 		<script>$(document).foundation();</script>
 		
 	</body>
